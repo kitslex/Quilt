af06232e96ce9260e6c65f87fbc7e1a16f09ef1e
uwuvern <uwuvern@outlook.com>
2024-04-06 08:47:10 +0100

    Initial commit

--- orbit/build.gradle.kts
+++ orbit/build.gradle.kts
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) Ashley (uwuvern) <uwuvern@outlook.com> 2024
+ *
+ * This project is licensed under the Apache 2.0 license, see the LICENSE file in the root of the project for more information.
+ *
+ * If you wish to contact me you can do so at <uwuvern@outlook.com>.
+ */
+
 plugins {
     id("java")
 }
